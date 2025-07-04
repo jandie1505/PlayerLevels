396e9e64e3e7f18427d95ba3dc3aee740024b41d
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2025-07-04 15:17:39 +0200

    Moved CloudNet Integration into the plugin itself instead of using a separate integration plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "PlayerLevels"
 
-include("PlayerLevels-Core", "PlayerLevels-API", "PlayerLevels-PlaceholderAPI-Expansion", "PlayerLevels-CloudNet")
+include("PlayerLevels-Core", "PlayerLevels-API", "PlayerLevels-PlaceholderAPI-Expansion")
